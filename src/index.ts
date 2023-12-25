e79bbb4bb97aaef60b94344d99769d5209ba3466
Jonathan Gil <jhongilp@gmail.com>
2023-12-25 12:19:21 -0500

    integrate schema created from Pothos

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
 import { createServer } from "node:http";
-import { createSchema, createYoga } from "graphql-yoga";
+import { createYoga } from "graphql-yoga";
+import { schema } from "./schema";
+
 const port = Number(process.env.API_PORT) || 4000;
-const schema = createSchema({
-  typeDefs: /* GraphQL */ `
-    type Query {
-      hello: String
-    }
-  `,
-  resolvers: {
-    Query: {
-      hello: () => "world",
-    },
-  },
-});
 
 const yoga = createYoga({ schema });
+
 const server = createServer(yoga);
+
 server.listen(port, () => {
   console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
 });
