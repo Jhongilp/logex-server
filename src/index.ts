725a7429e368858b0012f8c464cb4a3d5c579adf
Jonathan Gil <jhongilp@gmail.com>
2024-01-04 09:08:37 -0500

    remove pothos dependecy and start using prisma and grapqhQL manually

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { createServer } from "node:http";
 import { createYoga } from "graphql-yoga";
+// import { createSchema } from "graphql-yoga";
 import { schema } from "./schema";
 
-const port = Number(process.env.API_PORT) || 4000;
-
-const yoga = createYoga({ schema });
-
-const server = createServer(yoga);
-
-server.listen(port, () => {
-  console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
-});
+function main() {
+  const yoga = createYoga({ schema })
+  const server = createServer(yoga)
+  server.listen(4000, () => {
+    console.info('Server is running on http://localhost:4000/graphql')
+  })
+}
+ 
+main()
--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,61 @@
-import { builder } from "./builder";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { prisma } from "./db";
 
-import "./models/Customer";
-import "./models/Expo";
-import "./models/Shipping";
-import "./models/User";
+// https://the-guild.dev/graphql/yoga-server/tutorial/basic/03-graphql-server
 
+const typeDefinitions = /* GraphQL */ `
+  type Query {
+    hello: String!
+    users: [User]
+    customers: [Customer]
+  }
+  type User {
+    nit: String!
+    name: String!
+    customers: [Customer!]
+  }
 
-export const schema = builder.toSchema({});
+  type Customer {
+    id: ID!
+    name: String!
+    country: String!
+    city: String!
+    address: String!
+    user: User!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello World!",
+    users: async () => {
+      return prisma.user.findMany();
+    },
+    customers: async () => {
+      return prisma.customer.findMany();
+    },
+  },
+
+  User: {
+    customers: (user: any) =>
+      prisma.customer.findMany({
+        where: {
+          userId: user.nit,
+        },
+      }),
+  },
+
+  Customer: {
+    user: (customer: any) =>
+      prisma.user.findUnique({
+        where: {
+          nit: customer.userId,
+        },
+      }),
+  },
+};
+
+export const schema = makeExecutableSchema({
+  resolvers: [resolvers],
+  typeDefs: [typeDefinitions],
+});
